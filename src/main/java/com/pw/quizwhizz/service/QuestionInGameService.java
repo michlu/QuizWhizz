c4df30189f64fab98f477210db4e67948447d53a
Karolina Prusaczyk <karolina.prusaczyk@gmail.com>
2017-04-30 21:56:29 +0200

    Implemented and tested Question > QuestionInGame conversion

--- src/main/java/com/pw/quizwhizz/service/QuestionInGameService.java
+++ src/main/java/com/pw/quizwhizz/service/QuestionInGameService.java
@@ -1,7 +1,17 @@
 package com.pw.quizwhizz.service;
 
+import com.pw.quizwhizz.model.question.Question;
+import com.pw.quizwhizz.model.question.QuestionInGameDTO;
+
+import java.util.List;
+
 /**
- * Created by karol on 30.04.2017.
+ * Created by Karolina on 30.04.2017.
  */
 public interface QuestionInGameService {
+
+    List<QuestionInGameDTO> findAll();
+    QuestionInGameDTO findQuestionInGameByGameId(Long gameId);
+    List<QuestionInGameDTO> convertToQuestionsInGame(List<Question> questions, Long gameId);
+    void saveQuestionsInGame(List<QuestionInGameDTO> questions);
 }
--- src/main/java/com/pw/quizwhizz/service/impl/QuestionInGameServiceImpl.java
+++ src/main/java/com/pw/quizwhizz/service/impl/QuestionInGameServiceImpl.java
@@ -1,19 +1,52 @@
-package com.pw.quizwhizz.service.impl;public class QuestionInGameServiceImpl{com.pw.quizwhizz.repository.QuestionInGameRepository questionInGameRepository;	public QuestionInGameServiceImpl()	{	}@java.lang.Override
-    public com.pw.quizwhizz.model.entity.QuestionInGameDTO findQuestionInGameByGameId(java.lang.Long gameId) {
+package com.pw.quizwhizz.service.impl;
+
+import com.pw.quizwhizz.model.question.Question;
+import com.pw.quizwhizz.model.question.QuestionInGameDTO;
+import com.pw.quizwhizz.repository.QuestionInGameRepository;
+import com.pw.quizwhizz.service.QuestionInGameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionInGameServiceImpl implements QuestionInGameService {
+
+    private QuestionInGameRepository questionInGameRepository;
+
+    @Autowired
+    public QuestionInGameServiceImpl(QuestionInGameRepository questionInGameRepository) {
+        this.questionInGameRepository = questionInGameRepository;
+    }
+
+
+    @Override
+    public List<QuestionInGameDTO> findAll() {
+        return questionInGameRepository.findAll();
+    }
+
+    @Override
+    public QuestionInGameDTO findQuestionInGameByGameId(Long gameId) {
         return questionInGameRepository.findByGameId(gameId);
-    }@java.lang.Override
-    public java.util.List<com.pw.quizwhizz.model.entity.QuestionInGameDTO> convertToQuestionsInGame(java.util.List<com.pw.quizwhizz.model.entity.Question> questions, java.lang.Long gameId) {
-        java.util.List<com.pw.quizwhizz.model.entity.QuestionInGameDTO> questionsInGame = new java.util.ArrayList<com.pw.quizwhizz.model.entity.QuestionInGameDTO>();
+    }
+
+    @Override
+    public List<QuestionInGameDTO> convertToQuestionsInGame(List<Question> questions, Long gameId) {
+        List<QuestionInGameDTO> questionsInGame = new ArrayList<QuestionInGameDTO>();
 
         for (int i = 0; i < questions.size(); i++) {
-            com.pw.quizwhizz.model.entity.Question q = questions.get(i);
-            com.pw.quizwhizz.model.entity.QuestionInGameDTO question = new com.pw.quizwhizz.model.entity.QuestionInGameDTO(q, gameId, i);
+            Question q = questions.get(i);
+            QuestionInGameDTO question = new QuestionInGameDTO(q, gameId, i);
             questionsInGame.add(question);
         }
-            return questionsInGame;
-    }@java.lang.Override
-    public void saveQuestionsInGame(java.util.List<com.pw.quizwhizz.model.entity.QuestionInGameDTO> questions) {
-        for (com.pw.quizwhizz.model.entity.QuestionInGameDTO question : questions) {
+        return questionsInGame;
+    }
+
+    @Override
+    public void saveQuestionsInGame(List<QuestionInGameDTO> questions) {
+        for (QuestionInGameDTO question : questions) {
             questionInGameRepository.save(question);
         }
-    }}
+    }
+}
